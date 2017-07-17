6134d2d7712551332c145275959f6405370e5338
1378399640@qq.com <miao606653>
2017-07-17 17:48:02 +0800

    spring boot demo

---
+++ src/main/java/com/cs/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.cs.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    public String helloworld(){
+        return "Hello world!";
+    }
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+var hello = function () {
+    "use strict"
+    console.log("hello Spring boot");
+
+}
+
+hello();
